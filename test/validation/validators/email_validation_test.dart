871e1c8125f4717db97383b21c7065f0886cabf1
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-04 00:16:56 +0100

    move move emailValidation n to its own file

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,20 +1,5 @@
-import 'package:flutter_tdd_clean_architecture/validation/protocols/protocols.dart';
+import 'package:flutter_tdd_clean_architecture/validation/validators/validators.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class EmailValidation implements FieldValidation {
-  final String field;
-  EmailValidation(this.field);
-
-  String validate(String value) {
-
-    final regex = RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-        
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-
-    return isValid ? null : 'Campo invavido';
-  }
-}
 
 void main() {
   EmailValidation sut;
