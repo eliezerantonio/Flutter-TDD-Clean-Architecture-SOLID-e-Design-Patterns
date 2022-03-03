2d2e7746325cf2814da28f190f77bd61ab62db9c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-03 19:46:26 +0100

    Ensure StreamLoginPresenter emits form valid event if form is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -95,7 +95,19 @@ void main() {
     sut.isFormValidStream
         .listen(expectAsync1((isValid) => expect(isValid, false)));
 
-    sut.validateEmail(password);
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+  });
+  test('Should emit password error if validation fails ', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.passwordErrorStream
+        .listen(expectAsync1((error) => expect(error, null)));
+
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
     sut.validatePassword(password);
   });
 }
