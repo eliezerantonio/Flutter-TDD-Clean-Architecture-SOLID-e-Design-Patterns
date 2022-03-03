e3301ba5b6390e1fa3303add27c9589f2e26a637
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-03-03 20:38:39 +0100

    ensure StreamLoginPresenter calls Authentication with correct values

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import '../entities/entities.dart';
@@ -6,10 +7,12 @@ abstract class Authentication {
   Future<AccountEntity> auth(AuthenticationParams params);
 }
 
-class AuthenticationParams {
+class AuthenticationParams extends Equatable {
   final String email;
   final String secret;
 
   AuthenticationParams({@required this.email, @required this.secret});
 
+  @override
+  List<Object> get props => [email, secret];
 }
--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 
+import '../../domain/usecases/usecases.dart';
 import 'protocols/protocols.dart';
 
 class LoginState {
+  
   String email;
   String password;
   String emailError;
   String passwordError;
+
+
   bool get isFormValid =>
       emailError == null &&
       passwordError == null &&
@@ -17,7 +21,11 @@ class LoginState {
 }
 
 class StreamLoginPresenter {
+  StreamLoginPresenter(
+      {@required this.authentication, @required this.validation});
+
   final Validation validation;
+  final Authentication authentication;
   final _controller = StreamController<LoginState>.broadcast();
 
   var _state = LoginState();
@@ -29,8 +37,6 @@ class StreamLoginPresenter {
 
   Stream<bool> get isFormValidStream =>
       _controller.stream.map((state) => state.isFormValid).distinct();
-
-  StreamLoginPresenter({@required this.validation});
 
   void _update() => _controller.add(_state);
   void validateEmail(String email) {
@@ -44,5 +50,10 @@ class StreamLoginPresenter {
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();
+  }
+
+  Future<void> auth() async {
+    await authentication.auth(
+        AuthenticationParams(email: _state.email, secret: _state.password));
   }
 }
