aeb0ed6fa1da0bfa957c03c6c32b497b5506cb89
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-04-05 22:36:41 +0100

    Hide Key board tap

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -15,6 +15,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  void _hideKeyboard() {
+    final currectFocus = FocusScope.of(context);
+    if (!currectFocus.hasPrimaryFocus) {
+      currectFocus.unfocus();
+    }
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -41,40 +48,44 @@ class _LoginPageState extends State<LoginPage> {
           }
         });
 
-        return SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              LoginHeader(),
-              Headline1("Login"),
-              Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Provider(
-                    create: (_) => widget.presenter,
-                    child: Form(
-                      child: Column(
-                        children: [
-                          EmailInput(),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8, bottom: 32),
-                            child: PasswordInput(),
-                          ),
-                          LoginButton(),
-                          TextButton.icon(
-                            onPressed: () {},
-                            icon: Icon(Icons.person,
-                                color: Theme.of(context).primaryColor),
-                            label: Text(
-                              'Criar conta',
-                              style: TextStyle(
-                                  color: Theme.of(context).primaryColor),
+        return GestureDetector(
+          onTap: _hideKeyboard,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                LoginHeader(),
+                Headline1("Login"),
+                Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: Provider(
+                      create: (_) => widget.presenter,
+                      child: Form(
+                        child: Column(
+                          children: [
+                            EmailInput(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 8, bottom: 32),
+                              child: PasswordInput(),
                             ),
-                          )
-                        ],
+                            LoginButton(),
+                            TextButton.icon(
+                              onPressed: () {},
+                              icon: Icon(Icons.person,
+                                  color: Theme.of(context).primaryColor),
+                              label: Text(
+                                'Criar conta',
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  )),
-            ],
+                    )),
+              ],
+            ),
           ),
         );
       }),
