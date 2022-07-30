5baafe4b6425fa02833b9ba4ea96ee2f2ae9c435
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-07-30 06:17:24 +0100

    Move GetxPresenter on own file

---
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import '../../domain/usecases/usecases.dart';
+import '../../ui/pages/pages.dart';
+
+class GetxSplashPresenter implements SplashPresenter {
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+  LoadCurrentAccount loadCurrentAccount;
+
+  var _navigateTo = RxString('');
+
+  @override
+  Future<void> checkAccount() async {
+    try {
+      final account = await loadCurrentAccount.load();
+      _navigateTo.value = account == null ? '/login' : '/surveys';
+    } catch (e) {
+      _navigateTo.value = '/login';
+    }
+  }
+
+  @override
+  Stream<String> get navigateToStream => _navigateTo.stream;
+}
--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,31 +1,11 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_tdd_clean_architecture/domain/entities/entities.dart';
 import 'package:flutter_tdd_clean_architecture/domain/usecases/load_current_account.dart';
-import 'package:flutter_tdd_clean_architecture/ui/pages/pages.dart';
+import 'package:flutter_tdd_clean_architecture/presentation/presenters/getx_splash_presenter.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/state_manager.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 
-class GetxSplashPresenter implements SplashPresenter {
-  GetxSplashPresenter({@required this.loadCurrentAccount});
-  LoadCurrentAccount loadCurrentAccount;
 
-  var _navigateTo = RxString('');
-
-  @override
-  Future<void> checkAccount() async {
-    try {
-      final account = await loadCurrentAccount.load();
-      _navigateTo.value = account == null ? '/login' : '/surveys';
-    } catch (e) {
-       _navigateTo.value =  '/login';
-    }
-  }
-
-  @override
-  Stream<String> get navigateToStream => _navigateTo.stream;
-}
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
