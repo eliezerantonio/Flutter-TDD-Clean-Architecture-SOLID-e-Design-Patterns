fe0901b29612c73bcdea5b99dfdaa04956fd794c
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-09-16 04:41:07 +0100

    Ensure RemoteAddAccount throws UnexpectedError on 400

--- lib/data/usecases/add_account/remote_add_account.dart
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -1,5 +1,6 @@
 import 'package:meta/meta.dart';
 
+import '../../../domain/helpers/helpers.dart';
 import '../../../domain/usecases/usecases.dart';
 
 import '../../http/http.dart';
@@ -11,7 +12,12 @@ class RemoteAddAccount {
 
   Future<void> add(AddAccountParams params) async {
     final body = RemoteAddAccountParams.fromDomain(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError catch (error) {
+      throw DomainError.unexpected;
+    }
   }
 }
 
--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_tdd_clean_architecture/data/usecases/usecases.dart';
+import 'package:flutter_tdd_clean_architecture/domain/helpers/helpers.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -15,6 +16,23 @@ void main() {
   String url;
   AddAccountParams params;
 
+
+    Map mockValidData() =>
+      {'accessToken': faker.guid.guid(), 'name': faker.person.name()};
+      
+  PostExpectation mockRequest() => when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body')));
+
+  void mockHttpData(Map data) {
+    mockRequest().thenAnswer((_) async => data);
+  }
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
+
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
@@ -25,23 +43,29 @@ void main() {
         password: faker.internet.password(),
         passwordConfirmation: faker.internet.password());
   });
-  test(
-    'Should call HttpClient with currect Values',
-    () async {
-      await sut.add(params);
 
-      verify(
-        httpClient.request(
-          url: url,
-          method: 'post',
-          body: {
-            'name': params.name,
-            'email': params.email,
-            'password': params.password,
-            'passwordConfirmation': params.passwordConfirmation,
-          },
-        ),
-      );
-    },
-  );
+  
+  test('Should call HttpClient with currect Values', () async {
+    await sut.add(params);
+
+    verify(
+      httpClient.request(
+        url: url,
+        method: 'post',
+        body: {
+          'name': params.name,
+          'email': params.email,
+          'password': params.password,
+          'passwordConfirmation': params.passwordConfirmation,
+        },
+      ),
+    );
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () {
+    mockHttpError(HttpError.badRequest);
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
