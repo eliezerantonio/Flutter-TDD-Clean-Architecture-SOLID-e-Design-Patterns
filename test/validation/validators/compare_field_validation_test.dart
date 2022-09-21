c24120d6048e2670de2a953fc4e9c5f781c804d7
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-09-21 12:20:31 +0100

    ensure CompareFieldVlaidation retunr null if values are  equal

--- test/validation/validators/compare_field_validation_test.dart
+++ test/validation/validators/compare_field_validation_test.dart
@@ -15,7 +15,7 @@ class CompareFieldValidation implements FieldValidation {
   @override
   ValidationError validate(String value,) {
 
-  return ValidationError.invalidField;
+   return  value.contains(valueToCompare) ? null: ValidationError.invalidField;
 
   }
 
@@ -31,10 +31,15 @@ void main() {
 
     sut= CompareFieldValidation(field: 'any_field', valueToCompare: 'any_value');
   });
-  test('Should return error if value is not equal', () {
+  test('Should return error if values are not equal', () {
     
 
     expect(sut.validate('wrong_value'), ValidationError.invalidField);
+  });
+  test('Should return null if values are equal', () {
+    
+
+    expect(sut.validate('any_value'), null);
   });
  
 }
