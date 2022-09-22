e77aad42dbefa63aa3a2a1a959df417638645777
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-09-22 01:37:41 +0100

    move methods to setUp method

--- test/data/usecases/load_serveys/load_surveys_test.dart
+++ test/data/usecases/load_serveys/load_surveys_test.dart
@@ -18,11 +18,17 @@ class RemoteLoadSurveys {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should calll HttpClient with currect values', () async {
-    final url = faker.internet.httpUrl();
-    final httpClient = HttpClientSpy();
-    final sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
+  String url;
+  HttpClientSpy httpClient;
+  RemoteLoadSurveys sut;
 
+  setUp(() {
+   url= faker.internet.httpUrl();
+   httpClient=HttpClientSpy();
+   sut= RemoteLoadSurveys(url: url, httpClient: httpClient);
+  });
+
+  test('Should calll HttpClient with currect values', () async {
     await sut.load();
 
     verify(httpClient.request(url: url, method: 'get'));
