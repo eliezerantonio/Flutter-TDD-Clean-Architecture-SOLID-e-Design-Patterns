60fa7e388c22a23d0c4b6530b31fe225563782f9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-09-22 21:02:21 +0100

    Ensure HttpAdapter throws NotFoundError 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -245,5 +245,15 @@ void main() {
 
       expect(future, throwsA(HttpError.forbidden));
     });
+
+       test('Should return NotFoundError if get returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.notFound));
+    });
   });
+
+  
 }
