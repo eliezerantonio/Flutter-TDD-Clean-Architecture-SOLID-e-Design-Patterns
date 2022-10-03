0870b2f5ba06c4b6a474d00e6a466f7b3e8d9b9e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-10-03 10:42:21 +0100

    Ensure LocalStorageAdapter with correct values

---
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -0,0 +1,35 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:meta/meta.dart';
+
+class LocalStorageAdapter {
+  final LocalStorage localStorage;
+
+  LocalStorageAdapter({@required this.localStorage});
+
+
+  Future<void> save({@required String key, @required dynamic value}) async {
+   await localStorage.setItem(key, value);
+
+  }
+
+
+
+}
+
+class LocalStorageSpy extends Mock implements LocalStorage {}
+
+void main() {
+  test('Should call localStorage with correct values', () async {
+    final key = faker.randomGenerator.string(5);
+    final value = faker.randomGenerator.string(50);
+    final localStorage = LocalStorageSpy();
+    final sut = LocalStorageAdapter(localStorage: localStorage);
+
+    await sut.save(key: key, value: value);
+
+    verify(localStorage.setItem(key, value)).called(1);
+  });
+}
