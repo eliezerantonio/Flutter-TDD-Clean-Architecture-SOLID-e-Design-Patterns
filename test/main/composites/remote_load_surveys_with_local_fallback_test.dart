ad799b12a23eb35ac3b8f1857144792d4e20a154
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-10-05 23:40:33 +0100

    Move Sut creation to the setup method

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -2,30 +2,31 @@ import 'package:flutter_tdd_clean_architecture/data/usecases/usecases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
-class RemoteLoadSurveysWithLocalFallback{
 
+class RemoteLoadSurveysWithLocalFallback {
   final RemoteLoadSurveys remoteLoadSurveys;
 
   RemoteLoadSurveysWithLocalFallback({@required this.remoteLoadSurveys});
 
-  Future<void> load()async{
+  Future<void> load() async {
     remoteLoadSurveys.load();
   }
 }
 
-class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys{
+class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
-}
+void main() {
+  RemoteLoadSurveysSpy remote;
 
- void main(){
-  test('Should call remote load',()async{
+  RemoteLoadSurveysWithLocalFallback sut;
+  setUp(() {
+    remote = RemoteLoadSurveysSpy();
 
-    final remote =RemoteLoadSurveysSpy();
-
-    final sut=RemoteLoadSurveysWithLocalFallback(remoteLoadSurveys:remote);
-
+    sut = RemoteLoadSurveysWithLocalFallback(remoteLoadSurveys: remote);
+  });
+  test('Should call remote load', () async {
     await sut.load();
 
     verify(remote.load()).called(1);
-    });
- }
+  });
+}
